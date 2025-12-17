dfcd1eb037fa22ef032e0de7668bb93913e38bda
Michael Shibut <79378430+mishgan325@users.noreply.github.com>
2025-12-17 16:37:53 +0300

    Update CustomerClient.java

--- service/order-service/src/main/java/ru/mishgan325/docsa/pr8/client/CustomerClient.java
+++ service/order-service/src/main/java/ru/mishgan325/docsa/pr8/client/CustomerClient.java
@@ -4,6 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import ru.mishgan325.docsa.pr8.dto.CustomerDto;
+import ru.mishgan325.docsa.pr8.config.FeignConfig;
+
 
 @FeignClient(name = "customer-service", configuration = FeignConfig.class)
 public interface CustomerClient {
